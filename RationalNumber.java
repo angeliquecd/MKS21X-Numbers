f951dff976a8ba58796ff305a332bc0a436d10bd
angeliquecd <acharles-davis00@stuy.edu>
2018-10-16 17:34:14 -0400

    changes

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,20 +1,4 @@
 public class RationalNumber extends RealNumber{
-private int a;
-private int b;
-public RationalNumber(one,two){
-  a=one;
-  b=two;
-}
-public String toString(){
-  return "hello"
-}
-public boolean greaterthan(a){
-  return true;
-}
-public boolean lessthan(a){
-  return true;
-}
-public boolean equals(a){
-  return false;
-}
+private int numberator, denominator;
+
 }
