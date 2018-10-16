5d3979da6262e59e879611487fa694d69d0ec18d
angeliquecd <acharles-davis00@stuy.edu>
2018-10-15 20:37:56 -0400

    added first class

---
+++ RationalNumber.java
@@ -0,0 +1,20 @@
+public class RationalNumber extends RealNumber{
+private int a;
+private int b;
+public RationalNumber(one,two){
+  a=one;
+  b=two;
+}
+public String toString(){
+  return "hello"
+}
+public boolean greaterthan(a){
+  return true;
+}
+public boolean lessthan(a){
+  return true;
+}
+public boolean equals(a){
+  return false;
+}
+}
---
+++ RealNumber.java
@@ -0,0 +1,21 @@
+public class RealNumber{
+private double num;
+public RealNumber(x){
+  num=x;
+}
+public String toString(){
+return "hey";
+}
+public RealNumber add(a){
+  return new RealNumber(2);
+}
+public RealNumber subtract(a){
+  return new RealNumber(2);
+}
+public RealNumber multiply(a){
+  return new RealNumber(2);
+}
+public RealNumber divide(a){
+  return new RealNumber(3);
+}
+}
